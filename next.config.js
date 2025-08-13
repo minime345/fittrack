58300a1519edf33fd4b7c6cfd981d08dc5033c98
minime345 <martin_semov@abv.bg>
2025-08-14 02:42:35 +0300

    Switch Next.js config to JS for Vercel compatibility

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // ✅ Allow production builds to complete even if there are ESLint errors.
     ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // ✅ Allow production builds to complete even if there are TypeScript errors.
-    //    Use with caution — better to fix errors when possible.
-    ignoreBuildErrors: true,
   },
 };
 
